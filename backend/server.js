8245ead7dc4913e7a11493cf3d2ef3bf8a963247
Aqazde <adil64489@gmail.com>
2025-05-18 20:06:15 +0500

    feat: serve static files

--- backend/server.js
+++ backend/server.js
@@ -34,8 +34,17 @@ app.use('/api/quizzes', quizRoutes);
 app.use('/api/quizzes', quizSubmissionRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/dashboard', dashboardRoutes);
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Redirect all unmatched routes to index.html (for SPA-like routing)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
 app.use((req, res) => {
     res.status(404).json({ message: 'Route not found' });
 });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
